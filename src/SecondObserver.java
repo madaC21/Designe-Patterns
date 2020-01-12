5a09b6c01df762d01823e5e5a774afde5206aaad
madaC21 <56270005+madaC21@users.noreply.github.com>
2020-01-12 12:44:54 +0200

    Add files via upload

---
+++ src/SecondObserver.java
@@ -0,0 +1,10 @@
+
+public class SecondObserver implements Observer{
+
+	@Override
+	public void update(String oldValue, String newValue) {
+		System.out.println("From SecondObserver "+ oldValue + "   "+ newValue);
+		
+	}
+	
+}
---
+++ src/StatisticsCommand.java
@@ -0,0 +1,17 @@
+
+public class StatisticsCommand implements Command{
+
+	@Override
+	public void execute() {
+		BookStatistics stats = new BookStatistics();
+		DocumentManager.getInstance().getBook().accept(stats);
+		stats.printStatistics();
+	}
+
+	@Override
+	public void unexecute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
